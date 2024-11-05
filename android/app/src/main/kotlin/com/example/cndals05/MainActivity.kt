dddf70eee3229d49af3897d503be562d81271d33
cndals05 <121500090+cndals05@users.noreply.github.com>
2024-11-05 13:47:10 +0900

    첫 번째 커밋

---
+++ android/app/src/main/kotlin/com/example/cndals05/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cndals05
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ccccc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int score;
+  scanf("값: %d",&score);
+  if(score % 2 == 0){
+    printf("짝수");
+  }else{
+    printf("홀수");
+  }
+    
+    
+    return 0;
+}
